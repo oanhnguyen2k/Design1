1f732a7414a538ca143217e987b30b5b6ba52d2c
oanhnguyen2k <72710229+oanhnguyen2k@users.noreply.github.com>
2020-11-15 16:40:42 -0800

    Update url.h

--- url.h
+++ url.h
@@ -1,24 +1,27 @@
 #ifndef _URL_H_
 #define _URL_H_
 
-using namespace std;
+#include <string>
 
-class URL {
+class url {
   public:
     // default constructor
     url();
     
     // normal constructor
-    url(string link);
+    url(std::string link);
     
     // getters
-    string getFull();
-    string getScheme();
-    string getAuthority();
-    string getPath();
+    std::string getFull();
+    std::string getScheme();
+    std::string getAuthority();
+    std::string getPath();
   
   private:
-    string link_, scheme_, authority_, path_;
+    std::string link_;
+    std::string scheme_;
+    std::string authority_;
+    std::string path_;
 };
 
 #endif   // _URL_H_
